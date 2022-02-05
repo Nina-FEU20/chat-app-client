bc6a3627c2fc9e315b4e931c2b4f634d89eb19e6
Nina Jivetorp <ninajivetorp@gmail.com>
2022-02-05 02:18:20 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      teal100: '#DFEFEF',
+      teal200: '#A2D2D2',
+      teal300: '#488D8D',
+      teal400: '#316464',
+      teal500: '#132121',
+      teal600: '#0A0E0E',
+      pink100: '#C0A8C9',
+      pinkgrey: '#c0a8c92b',
+      red: '#FF0000',
+      white: '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
