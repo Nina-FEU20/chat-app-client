52d3b12a5dfac4ab8652d3798cb654dcafedd677
Nina Jivetorp <ninajivetorp@gmail.com>
2022-02-22 14:00:31 +0100

    Add darkmode toggle, small fixes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,14 +5,13 @@ import { BsChatDotsFill } from 'react-icons/bs';
 import avatar from '../assets/avatar.png';
 import Button from './Button';
 import AccountModal from './AccountModal';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
 
 const Nav = ({ classnames }) => {
   const [open, setOpen] = useState(false);
   const { authUser, setAuthUser } = AuthState();
   const [darkMode, setDarkMode] = useState(false);
-
-  const handleDarkMode = () => {};
-  console.log(darkMode);
+  const toggleClass = ' transform translate-x-6';
 
   useEffect(() => {
     console.log('hey');
@@ -45,19 +44,40 @@ const Nav = ({ classnames }) => {
       <div className='flex justify-between items-center py-2 2xl:py-4'>
         <div className='flex items-center'>
           <BsChatDotsFill className='text-teal200 text-5xl' />
-          <h2 className='pl-2 text-xl sm:text-2xl dark:text-white'>Talk Daily</h2>
+          <h2 className='text-base pl-2 sm:text-xl sm:text-2xl dark:text-white'>ChatDaily</h2>
         </div>
         {authUser ? (
-          <div className='flex space-x-2 sm:space-x-6 items-center'>
-            <Button onClick={() => setDarkMode(!darkMode)}>Dark Mode</Button>
-            <p className='font-semibold dark:text-white'>{authUser.username}</p>
-            <img src={avatar} alt='avatar' className='h-10 border border-teal200 rounded-full p-[1px]' />
-            <Button onClick={(e) => handleSignOut(e)}>Logout</Button>
+          <div className='flex space-x-2 sm:space-x-4 items-center'>
+            <div
+              className='w-14 h-7 flex items-center bg-teal200 dark:bg-teal400 rounded-full p-1 cursor-pointer relative -mr-2 sm:mr-0'
+              onClick={() => {
+                setDarkMode(!darkMode);
+              }}
+            >
+              <BsFillMoonStarsFill className='absolute left-2 text-teal500  ' />
+              <BsFillSunFill className='absolute right-2 dark:text-yellow' />
+              {/* Switch */}
+              <div
+                className={
+                  'bg-teal400 dark:bg-teal600 w-6 h-6  rounded-full shadow-md transform duration-300 ease-in-out' + (darkMode ? null : toggleClass)
+                }
+              ></div>
+            </div>
+            <div className='flex items-center space-x-2'>
+              <p className='hidden sm:block font-semibold dark:text-white'>{authUser.username}</p>
+              <img src={avatar} alt='avatar' className='h-10 border border-teal200 rounded-full p-[1px]' />
+            </div>
+
+            <Button classnames='text-xs px-2 sm:text-sm ' onClick={(e) => handleSignOut(e)}>
+              Logout
+            </Button>
           </div>
         ) : (
           <div className='flex space-x-2 sm:space-x-6 items-center'>
-            <Button onClick={() => setOpen('create')}>Sign up</Button>
-            <Button filled={true} onClick={() => setOpen('login')}>
+            <Button classnames='text-xs px-2 sm:text-base sm:px-4' onClick={() => setOpen('create')}>
+              Sign up
+            </Button>
+            <Button classnames='text-xs px-2 sm:text-base sm:px-4' filled={true} onClick={() => setOpen('login')}>
               Log in
             </Button>
           </div>
